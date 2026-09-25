02c4c36fcec8fd0fcf0bcee0db0c87eabc2eb0c3
bfeuillatusjfc-blip <bfeuillat.usjfc@gmail.com>
2026-09-25 16:29:18 +0200

    V8 disable stale app-shell cache during testing

--- sw.js
+++ sw.js
@@ -1,10 +1,4 @@
-const CACHE='match-notes-v7';
-const ASSETS=['./manifest.webmanifest'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
-self.addEventListener('fetch',e=>{
- if(e.request.mode==='navigate'||new URL(e.request.url).pathname.endsWith('/index.html')||new URL(e.request.url).pathname.endsWith('/match-notes/')){
-   e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));return;
- }
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+const CACHE='match-notes-v8';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
